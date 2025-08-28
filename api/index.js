b714a1278937b60fa75b61bec3efe33b3adb06fb
farihakausar <88570666+farihakausar@users.noreply.github.com>
2025-08-28 20:06:18 +0500

    OIUYUIOIUYT

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
-const serverless = require("serverless-http");
-const app = require("../src/app");
+const express = require("express");
+const app = express();
 
-module.exports = serverless(app);
+app.use(express.json());
+
+// Simple route
+app.get("/", (req, res) => {
+  res.json({ message: "🚀 Backend running on Vercel!" });
+});
+
+// Example POST
+app.post("/echo", (req, res) => {
+  res.json({ youSent: req.body });
+});
+
+// Export handler for Vercel
+module.exports = app;
